0f1c43ec8b56ca323d673332d0b15fd923eb7a8c
finni <nikalas.boyanov.nunev@cieep.com>
2023-10-26 14:03:34 +0200

    Initial Commit

---
+++ EjemploDevolverInformacionUITests/EjemploDevolverInformacionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EjemploDevolverInformacionUITestsLaunchTests.swift
+//  EjemploDevolverInformacionUITests
+//
+//  Created by Finni on 26/10/23.
+//
+
+import XCTest
+
+final class EjemploDevolverInformacionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
